48141f1ae74e055073c1001c286d5af8aa9904f4
Alexander Bird <alex@alexanderbird.software>
2024-05-03 20:39:59 -0700

    F** Add class for board state

--- index.js
+++ index.js
@@ -10,13 +10,25 @@ const dimensions = {
 function renderGame() {
   const board = Array.from({ length: dimensions.columns }).map((_, column) =>
     Array.from({ length: dimensions.rows }).map((_, row) => ({ row, column })));
+  const state = new GameState();
   return `
     <table>
       ${board.map(row => `
         <tr>
-          ${row.map(cell => `<td><div class='cell'></div></td>`).join('')}
+          ${row.map(cell => `<td><div class='cell ${state.getCell(cell)}'></div></td>`).join('')}
         </tr>
       `).join('')}
     </table>
   `;
+}
+
+class GameState {
+  constructor() {
+    this.boardThings = {
+    }
+  }
+    
+  getCell({ row, column }) {
+    return '';
+  }
 }
