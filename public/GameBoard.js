b29dde09a13c88a24dfe9536ee2628874910041b
Alexander Bird <alex@alexanderbird.software>
2024-05-15 20:07:43 -0700

    F** Use the Cell class in the GameBoard to prepare for adding cell contents

--- public/GameBoard.js
+++ public/GameBoard.js
@@ -1,11 +1,20 @@
 module.GameBoard = (async function main() {
   const { CellReference } = await dependsOn('CellReference');
+  const { Cell } = await dependsOn('Cell');
+
   class GameBoard {
     #cells;
 
     constructor() {
       this.#cells = Array.from({ length: 64 }).map((_, row) =>
-        Array.from({ length: 64 }).map((_, column) => new CellReference({ row, column })));
+        Array.from({ length: 64 }).map((_, column) => GameBoard.initializeCell(row, column)));
+    }
+
+    static initializeCell(row, column) {
+      const reference = new CellReference({ row, column });
+      return new Cell({
+        location: reference,
+      });
     }
 
     getCells() {
--- test/HtmlGameBoardRenderer.spec.js
+++ test/HtmlGameBoardRenderer.spec.js
@@ -4,14 +4,21 @@ const { describe } = require('./diyUnit');
 module.exports = (async function main() {
   const { HtmlGameBoardRenderer } = await diyRequire('HtmlGameBoardRenderer');
   const { CellReference } = await diyRequire('CellReference');
+  const { Cell } = await diyRequire('Cell');
+
+  function cell(row, column) {
+    return new Cell({
+      location: new CellReference({ row, column }),
+    });
+  }
 
   describe('HtmlGameBoardRenderer', ({ it }) => {
     it('renders all of the item types', ({ expect }) => {
       const renderer = new HtmlGameBoardRenderer();
       const fakeBoard = {
         getCells: () => [
-          [ new CellReference({ row: 0, column: 0 }), new CellReference({ row: 0, column: 1 }), new CellReference({ row: 0, column: 2 }),],
-          [ new CellReference({ row: 1, column: 0 }), new CellReference({ row: 1, column: 1 }), new CellReference({ row: 1, column: 2 }),],
+          [ cell(0, 0), cell(0, 1), cell(0, 2) ],
+          [ cell(1, 0), cell(1, 1), cell(1, 2) ],
         ]
       };
       const html = renderer.render(fakeBoard);
